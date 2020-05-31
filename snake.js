d85ae997703f14e0fb2b7b7255959d0e11003678
unknown <metkraming@gmail.com>
2020-05-31 20:18:32 +0300

    add field render to js file and add teleport move

--- snake.js
+++ snake.js
@@ -2,22 +2,32 @@
 class Snake {
   constructor() {
     this.snakeCoordinates = [[0, 5], [0, 6], [0, 7], [1, 7], [1, 8]];
+    this.field = document.createElement("div");
+    this.field.id = "field";
+    for (let i = 0; i < 100; i++) {
+      let block = document.createElement("div");
+      block.className = "empty-block";
+      block.innerHTML = i;
+      this.field.append(block);
+    }
+    this.blocks = this.field.querySelectorAll("div");
     // this.posX = 0;
     // this.posY = 5;
     // this.tail = [[this.posX, this.posY + 1], [this.posX, this.posY + 2]];
     this.direction = [0, -1];
     // this.position = this.snakeCoordinates[0][1] * 10 + this.snakeCoordinates[0][0];
     for (let i of this.snakeCoordinates) {
-      randomDiv[i[1] * 10 + i[0]].className = "snake-block";
+      this.blocks[i[1] * 10 + i[0]].className = "snake-block";
     }
 
+    document.body.append(this.field);
     // for (let i of this.tail) {
     //   randomDiv[10 * i[1] + i[0]].className = "block-black";
     // }
   }
   step() {
     // randomDiv[this.snakeCoordinates[1][1] * 10 + this.snakeCoordinates[1][0]].className = "block";
-    randomDiv[this.snakeCoordinates[this.snakeCoordinates.length - 1][1] * 10 + this.snakeCoordinates[this.snakeCoordinates.length - 1][0]].className = "empty-block";
+    this.blocks[this.snakeCoordinates[this.snakeCoordinates.length - 1][1] * 10 + this.snakeCoordinates[this.snakeCoordinates.length - 1][0]].className = "empty-block";
     //corner turn
     if (this.snakeCoordinates[0][0] == 0 && this.snakeCoordinates[0][1] == 0) {
       this.direction = [1, 0];
@@ -28,6 +38,7 @@ class Snake {
     } else if (this.snakeCoordinates[0][0] == 0 && this.snakeCoordinates[0][1] == 9) {
       this.direction = [0, -1];
     }
+
     //move all coordinates
     for (let i = this.snakeCoordinates.length - 1; i > 0 ; i--) {
       this.snakeCoordinates[i][0] = this.snakeCoordinates[i - 1][0];
@@ -36,15 +47,28 @@ class Snake {
     }
     this.snakeCoordinates[0][0] += this.direction[0];
     this.snakeCoordinates[0][1] += this.direction[1];
+    //teleport move
+    if (this.snakeCoordinates[0][0] == -1) {
+      this.snakeCoordinates[0][0] = 9;
+    }
+    if (this.snakeCoordinates[0][0] == 10) {
+      this.snakeCoordinates[0][0] = 0;
+    }
+    if (this.snakeCoordinates[0][1] == 10) {
+      this.snakeCoordinates[0][1] = 0;
+    }
+    if (this.snakeCoordinates[0][1] == -1) {
+      this.snakeCoordinates[0][1] = 9;
+    }
 
 
     // this.position = this.snakeCoordinates[0][1] * 10 + this.snakeCoordinates[0][0];
     //rendering of snake coordinates
     for (let i = 0; i < this.snakeCoordinates.length; i++) {
       if (i == 0) {
-        randomDiv[this.snakeCoordinates[0][1] * 10 + this.snakeCoordinates[0][0]].className = "head-snake-block";
+        this.blocks[this.snakeCoordinates[0][1] * 10 + this.snakeCoordinates[0][0]].className = "head-snake-block";
       } else {
-        randomDiv[this.snakeCoordinates[i][1] * 10 + this.snakeCoordinates[i][0]].className = "snake-block";
+        this.blocks[this.snakeCoordinates[i][1] * 10 + this.snakeCoordinates[i][0]].className = "snake-block";
       }
     }
     // for (let i of this.snakeCoordinates) {
@@ -56,6 +80,7 @@ class Snake {
 function changeDirection(event) {
   switch(event.code) {
     case "ArrowUp":
+      console.log("up");
       newSnake.direction = [0, -1];
       break;
     case "ArrowDown":
@@ -70,12 +95,9 @@ function changeDirection(event) {
   }
 }
 
-let randomDiv = document.querySelector("#field").querySelectorAll("div");
 let newSnake = new Snake();
 setInterval(() => newSnake.step(), 1000);
 document.addEventListener("keydown", changeDirection);
-
-
 
 function randomBlock() {
   let randomNum = Math.round(Math.random() * 100);
