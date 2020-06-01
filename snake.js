2216ca982413574f78f3ee86e9fa71f20cd83a72
unknown <metkraming@gmail.com>
2020-06-01 10:49:57 +0300

    add renderField function

--- snake.js
+++ snake.js
@@ -16,18 +16,38 @@ class Snake {
     // this.tail = [[this.posX, this.posY + 1], [this.posX, this.posY + 2]];
     this.direction = [0, -1];
     // this.position = this.snakeCoordinates[0][1] * 10 + this.snakeCoordinates[0][0];
-    for (let i of this.snakeCoordinates) {
-      this.blocks[i[1] * 10 + i[0]].className = "snake-block";
-    }
+    // for (let i = 0; i < this.snakeCoordinates.length; i++) {
+    //   if (i == 0) {
+    //     this.blocks[this.snakeCoordinates[i][1] * 10 + this.snakeCoordinates[i][0]].className = "head-snake-block";
+    //     continue;
+    //   }
+    //   this.blocks[this.snakeCoordinates[i][1] * 10 + this.snakeCoordinates[i][0]].className = "snake-block";
+    // }
+    this.fieldRender();
 
     document.body.append(this.field);
     // for (let i of this.tail) {
     //   randomDiv[10 * i[1] + i[0]].className = "block-black";
     // }
   }
+
+  fieldRender() {
+    for (let i = 0; i < 100; i++) {
+      this.blocks[i].className = "empty-block";
+    }
+
+    for (let i = 0; i < this.snakeCoordinates.length; i++) {
+      if (i == 0) {
+        this.blocks[this.snakeCoordinates[i][1] * 10 + this.snakeCoordinates[i][0]].className = "head-snake-block";
+        continue;
+      }
+      this.blocks[this.snakeCoordinates[i][1] * 10 + this.snakeCoordinates[i][0]].className = "snake-block";
+    }
+  }
+
   step() {
     // randomDiv[this.snakeCoordinates[1][1] * 10 + this.snakeCoordinates[1][0]].className = "block";
-    this.blocks[this.snakeCoordinates[this.snakeCoordinates.length - 1][1] * 10 + this.snakeCoordinates[this.snakeCoordinates.length - 1][0]].className = "empty-block";
+    //this.blocks[this.snakeCoordinates[this.snakeCoordinates.length - 1][1] * 10 + this.snakeCoordinates[this.snakeCoordinates.length - 1][0]].className = "empty-block";
     //corner turn
     if (this.snakeCoordinates[0][0] == 0 && this.snakeCoordinates[0][1] == 0) {
       this.direction = [1, 0];
@@ -64,13 +84,14 @@ class Snake {
 
     // this.position = this.snakeCoordinates[0][1] * 10 + this.snakeCoordinates[0][0];
     //rendering of snake coordinates
-    for (let i = 0; i < this.snakeCoordinates.length; i++) {
-      if (i == 0) {
-        this.blocks[this.snakeCoordinates[0][1] * 10 + this.snakeCoordinates[0][0]].className = "head-snake-block";
-      } else {
-        this.blocks[this.snakeCoordinates[i][1] * 10 + this.snakeCoordinates[i][0]].className = "snake-block";
-      }
-    }
+    this.fieldRender();
+    // for (let i = 0; i < this.snakeCoordinates.length; i++) {
+    //   if (i == 0) {
+    //     this.blocks[this.snakeCoordinates[0][1] * 10 + this.snakeCoordinates[0][0]].className = "head-snake-block";
+    //   } else {
+    //     this.blocks[this.snakeCoordinates[i][1] * 10 + this.snakeCoordinates[i][0]].className = "snake-block";
+    //   }
+    // }
     // for (let i of this.snakeCoordinates) {
     //   randomDiv[i[1] * 10 + i[0]].className = "snake-block";
     // }
@@ -96,7 +117,7 @@ function changeDirection(event) {
 }
 
 let newSnake = new Snake();
-setInterval(() => newSnake.step(), 1000);
+setInterval(() => newSnake.step(), 150);
 document.addEventListener("keydown", changeDirection);
 
 function randomBlock() {
